3f307a1cf37e169ae488a8dd4c16524fedf7ece8
fance068 <39392319+fance068@users.noreply.github.com>
2021-07-24 09:09:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-git clone https://github.com/small-5/luci-app-adblock-plus
+
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -16,3 +16,4 @@ git clone https://github.com/small-5/luci-app-adblock-plus
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
