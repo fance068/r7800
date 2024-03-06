9a542024f1cdeacaf9aec1ccd3814a29d02e3cfb
fance068 <39392319+fance068@users.noreply.github.com>
2024-03-06 22:34:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
